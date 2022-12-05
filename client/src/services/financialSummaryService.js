f08b80e424b3655f191a25a6c6f699e75a0a4fe7
DanielMalede <danielmalede@gmail.com>
2022-12-05 20:59:27 +0200

    .

--- client/src/services/financialSummaryService.js
+++ client/src/services/financialSummaryService.js
@@ -1,7 +1,7 @@
 export const financialSummaryData = async () => {
   try {
-    return await fetch("http://localhost:5000/financialSummary").then((res) => 
-    res.json()
+    return await fetch("http://localhost:5000/financialSummary").then((res) =>
+      res.json()
     );
   } catch (error) {
     console.log(error);
