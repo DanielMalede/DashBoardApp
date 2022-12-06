ddb4d341b05c2d150a4e36cc8674bf2a21396202
DanielMalede <danielmalede@gmail.com>
2022-12-06 12:44:38 +0200

    delete

--- server/validation/emailValidate.js
+++
@@ -1,8 +0,0 @@
- module.exports =  validateEmail = (email) => {
-    const temp = email.indexOf("@")
-    const last = email.slice(email.length-4)
-    if (temp > 0 && last == ".com" ){
-        return true
-    }
-    return false
-}
