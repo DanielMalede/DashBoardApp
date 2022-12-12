6a3b83fdec9a6fe6ff3076bc58d91ef694d5f088
DanielMalede <danielmalede@gmail.com>
2022-12-12 23:08:02 +0200

    .

--- server/model/products-model.js
+++ server/model/products-model.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const Products = new Schema({
-  id:{type:Number},
-  productName:{type:String},
-  color:{type:String},
-  num_in_stock:{type:Number}
+  id: { type: Number },
+  productName: { type: String },
+  color: { type: String },
+  num_in_stock: { type: Number },
 });
 
 module.exports = mongoose.model("Products", Products);
