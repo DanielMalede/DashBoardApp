30e68b54a9f8282fb77ad8abd5c8cca250afa364
DanielMalede <danielmalede@gmail.com>
2022-12-12 23:07:45 +0200

    .

--- server/model/infoPages-model.js
+++ server/model/infoPages-model.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const infoPages = new Schema({
-    id:{type:Number},
-    name:{type:String},
-    img:{type:String},
-    sold:{type:Number}
-},)
+  id: { type: Number },
+  name: { type: String },
+  img: { type: String },
+  sold: { type: Number },
+});
 
-module.exports = mongoose.model('infoPages', infoPages);
+module.exports = mongoose.model("infoPages", infoPages);
