2237e37d0128969cb50e10e4261770682526ca0c
DanielMalede <danielmalede@gmail.com>
2022-12-12 23:08:13 +0200

    .

--- server/model/users-model.js
+++ server/model/users-model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const Users = new Schema({
-    added_users:{type:Number}
+  added_users: { type: Number },
 });
 
 module.exports = mongoose.model("Users", Users);
