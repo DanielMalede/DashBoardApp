049eea3792c39294c2d23c93150a4bb2bd4f4608
DanielMalede <danielmalede@gmail.com>
2022-12-12 23:07:33 +0200

    .

--- server/model/financialSummary-model.js
+++ server/model/financialSummary-model.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const FinancialSummary = new Schema({
-  id:{type:Number},
-  profits:{type:Number},
-  losses:{type:Number}
+  id: { type: Number },
+  profits: { type: Number },
+  losses: { type: Number },
 });
 
 module.exports = mongoose.model("FinancialSummary", FinancialSummary);
